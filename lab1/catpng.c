0cb44baeed38f3d14792247e3c32defdba482c02
Edward Li <c455li@uwaterloo.ca>
2020-09-27 11:17:57 -0400

    fix memory leak

--- lab1/catpng.c
+++ lab1/catpng.c
@@ -21,7 +21,14 @@ U32 get_crc_value(struct chunk *chunk_p, struct data_IHDR *data_ihdr)
     {
         memcpy(source + CHUNK_TYPE_SIZE, chunk_p->p_data, get_chunk_length(chunk_p) * sizeof(U8));
     }
-    return crc(source, len_source);
+
+    U32 res = crc(source, len_source);
+    if (source != NULL)
+    {
+        free(source);
+        source = NULL;
+    }
+    return res;
 }
 
 void concat(struct chunk *res, struct simple_PNG **sub_pngs, struct data_IHDR **sub_data_ihdrs, int sub_png_len)
@@ -32,36 +39,50 @@ void concat(struct chunk *res, struct simple_PNG **sub_pngs, struct data_IHDR **
 
     for (int i = 0; i < sub_png_len; i++)
     {
-        printf("here\n");
-        printf("%x\n", get_chunk_length(sub_pngs[i]->p_IDAT));
         U32 height = get_png_height(sub_data_ihdrs[i]);
         U32 width = get_png_width(sub_data_ihdrs[i]);
         def_array[i] = (U8 *)malloc(height * (width * 4 + 1));
 
         mem_inf(def_array[i], &lengthes[i], sub_pngs[i]->p_IDAT->p_data, get_chunk_length(sub_pngs[i]->p_IDAT));
         final_length += lengthes[i];
-        printf("%X\n", final_length);
     }
 
     U8 *def_concat = (U8 *)malloc(sizeof(U8) * final_length);
     int prev_end = 0;
     for (int i = 0; i < sub_png_len; i++)
     {
-        printf("%d\n", i);
         memcpy(def_concat + prev_end, def_array[i], lengthes[i] * sizeof(U8));
         prev_end += lengthes[i];
     }
 
-    printf("fl %d\n", final_length);
-    printf("pe %d\n", prev_end);
-
     res->p_data = (U8 *)malloc(sizeof(U8) * final_length);
-    U32 result_length = 0;
-    int run_res = mem_def(res->p_data, &result_length, def_concat, final_length, Z_DEFAULT_COMPRESSION);
-    set_chunk_length(res, result_length);
-    if (run_res != 0)
+    U64 result_length = 0;
+    mem_def(res->p_data, &result_length, def_concat, final_length, Z_DEFAULT_COMPRESSION);
+    set_chunk_length(res, (U32)result_length);
+
+    if (lengthes != NULL)
     {
-        printf("error\n");
+        free(lengthes);
+        lengthes = NULL;
+    }
+    for (int i = 0; i < sub_png_len; i++)
+    {
+        if (def_array[i] != NULL)
+        {
+            free(def_array[i]);
+            def_array[i] = NULL;
+        }
+    }
+    if (def_array != NULL)
+    {
+        free(def_array);
+        def_array = NULL;
+    }
+
+    if (def_concat != NULL)
+    {
+        free(def_concat);
+        def_concat = NULL;
     }
 }
 
@@ -82,7 +103,6 @@ int main(int argc, char **argv)
 
     for (int i = 0; i < sub_png_len; i++)
     {
-        printf("argv[%d]=%s\n", i + 1, argv[i + 1]);
         char *sub_filename = argv[i + 1];
         FILE *sub_file = fopen(sub_filename, "rb");
 
@@ -94,20 +114,14 @@ int main(int argc, char **argv)
             set_png_width(data_ihdr, get_png_width(sub_data_ihdrs[i]));
         }
         set_png_height(data_ihdr, get_png_height(data_ihdr) + get_png_height(sub_data_ihdrs[i]));
-        printf("curr: %s: %u x %u\n", filename, get_png_width(data_ihdr), get_png_height(data_ihdr));
 
         sub_pngs[i] = (struct simple_PNG *)malloc(sizeof(struct simple_PNG));
         read_png(sub_pngs[i], sub_file);
-        printf("%s: %d x %d\n", sub_filename, get_png_width(sub_data_ihdrs[i]), get_png_height(sub_data_ihdrs[i]));
 
         set_chunk_length(png_data->p_IDAT, get_chunk_length(png_data->p_IDAT) + get_chunk_length(sub_pngs[i]->p_IDAT));
 
-        printf("%x\n", get_chunk_length(sub_pngs[i]->p_IDAT));
-
         fclose(sub_file);
     }
-
-    printf("out loop\n");
 
     fwrite(signature_buffer, PNG_SIG_SIZE, 1, file);
 
@@ -130,5 +144,103 @@ int main(int argc, char **argv)
     fwrite(&png_data->p_IEND->crc, CHUNK_CRC_SIZE, 1, file);
 
     fclose(file);
+
+    for (int i = 0; i < sub_png_len; i++)
+    {
+        if (sub_data_ihdrs[i] != NULL)
+        {
+            free(sub_data_ihdrs[i]);
+            sub_data_ihdrs[i] = NULL;
+        }
+
+        if (sub_pngs[i]->p_IHDR->p_data != NULL)
+        {
+            free(sub_pngs[i]->p_IHDR->p_data);
+            sub_pngs[i]->p_IHDR->p_data = NULL;
+        }
+        if (sub_pngs[i]->p_IDAT->p_data != NULL)
+        {
+            free(sub_pngs[i]->p_IDAT->p_data);
+            sub_pngs[i]->p_IDAT->p_data = NULL;
+        }
+        if (sub_pngs[i]->p_IEND->p_data != NULL)
+        {
+            free(sub_pngs[i]->p_IEND->p_data);
+            sub_pngs[i]->p_IEND->p_data = NULL;
+        }
+        if (sub_pngs[i]->p_IHDR != NULL)
+        {
+            free(sub_pngs[i]->p_IHDR);
+            sub_pngs[i]->p_IHDR = NULL;
+        }
+        if (sub_pngs[i]->p_IDAT != NULL)
+        {
+            free(sub_pngs[i]->p_IDAT);
+            sub_pngs[i]->p_IDAT = NULL;
+        }
+        if (sub_pngs[i]->p_IEND != NULL)
+        {
+            free(sub_pngs[i]->p_IEND);
+            sub_pngs[i]->p_IEND = NULL;
+        }
+        if (sub_pngs[i] != NULL)
+        {
+            free(sub_pngs[i]);
+            sub_pngs[i] = NULL;
+        }
+    }
+
+    if (data_ihdr != NULL)
+    {
+        free(data_ihdr);
+        data_ihdr = NULL;
+    }
+
+    if (png_data->p_IHDR->p_data != NULL)
+    {
+        free(png_data->p_IHDR->p_data);
+        png_data->p_IHDR->p_data = NULL;
+    }
+    if (png_data->p_IDAT->p_data != NULL)
+    {
+        free(png_data->p_IDAT->p_data);
+        png_data->p_IDAT->p_data = NULL;
+    }
+    if (png_data->p_IEND->p_data != NULL)
+    {
+        free(png_data->p_IEND->p_data);
+        png_data->p_IEND->p_data = NULL;
+    }
+    if (png_data->p_IHDR != NULL)
+    {
+        free(png_data->p_IHDR);
+        png_data->p_IHDR = NULL;
+    }
+    if (png_data->p_IDAT != NULL)
+    {
+        free(png_data->p_IDAT);
+        png_data->p_IDAT = NULL;
+    }
+    if (png_data->p_IEND != NULL)
+    {
+        free(png_data->p_IEND);
+        png_data->p_IEND = NULL;
+    }
+    if (png_data != NULL)
+    {
+        free(png_data);
+        png_data = NULL;
+    }
+    if (sub_pngs != NULL)
+    {
+        free(sub_pngs);
+        sub_pngs = NULL;
+    }
+    if (sub_data_ihdrs != NULL)
+    {
+        free(sub_data_ihdrs);
+        sub_data_ihdrs = NULL;
+    }
+
     return 0;
 }
